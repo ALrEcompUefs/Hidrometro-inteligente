635541ecac4dca85605ebcf4ba7b7dd5c94e1312
Alisson Rodrigues <43976401+ALrEcompUefs@users.noreply.github.com>
2022-09-06 09:53:03 -0300

    Update servidor.cpp

--- servidor.cpp
+++ servidor.cpp
@@ -12,6 +12,7 @@
 
 #define PORT 8080	// PORTA DO SERVDOR
 
+// para compilar g++ servidor.cpp -o servidor
 int main(int argc, char const* argv[])
 {
 	int pt_servidor;	//Descritor de arquivo do servidor
@@ -30,54 +31,69 @@ int main(int argc, char const* argv[])
 		|   SOCK_STREAM define comunicação orientada para conexão 
 		|   SOCK_DGRAM define comunicação não orientada a conexão
 		*/
-		if ((pt_servidor = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+
+		pt_servidor = socket(AF_INET, SOCK_STREAM, 0);
+		if ( pt_servidor== 0) {
 			printf("Falha na criação do socket\n");
 			exit(EXIT_FAILURE);
 		}
 
-		// Força a conexão do servidor na porta definida
+		bzero(&endereco, sizeof(endereco)); // zera espaço de mémoria para struct do endereço
+
+		/*  
+		|	Força a conexão do servidor na porta definida
+		|	pt_servidor é o socket mestre do sevidor
+		*/
 		if (setsockopt(pt_servidor, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
 			printf("Erro na conexão\n");
 			exit(EXIT_FAILURE);
 		}
 
+		/*
+		|	Insere os parametros do enderço na struct
+		*/
 		endereco.sin_family = AF_INET;			// Insere na struct
 		endereco.sin_addr.s_addr = INADDR_ANY;	// Insere na struct
 		endereco.sin_port = htons(PORT);		// traduz o inteiro sem sinal para  "network byte order" e insere na struct
 
-		// Vincula o servidor a porta definida, de modo forçado
+		/*
+		|	Vincula o servidor a porta definida, de modo forçado
+		*/ 
 		if (bind( pt_servidor , (struct sockaddr*)&endereco, sizeof(endereco)) < 0) {
 			printf("Falha na vinculação\n");
 			exit(EXIT_FAILURE);
 		}
-
+		printf("\n Servidor vinculado a porta %d",PORT);
 		/*
 		|	Nesta etapa a configuração do servidor ja foi feita e ele pode ser colocado em operação
 		*/
+		printf("\n----Servidor Iniciado----\n---- Aguardando conexões ---\n");
 
-		// Coloca o servidor em modo de escuta esperando conexões
-		printf("\n Servidor Iniciado\n");
-		if (listen(pt_servidor, 3) < 0) {
+		// Coloca o servidor em modo de escuta esperando conexões	
+		if (listen(pt_servidor, 5) < 0) {
 			printf("Erro na lista de espera\n");
 			exit(EXIT_FAILURE);
 		}
 
-		// Quando uma conexão é solicitada 
-		if ((novo_socket = accept(pt_servidor, (struct sockaddr*)&endereco, (socklen_t*)&addrlen)) < 0) {
-			printf("Erro de conexão com socket cliente\n");
-			exit(EXIT_FAILURE);
+		while(1){
+			
+			// Quando uma conexão é solicitada 
+			if ((novo_socket = accept(pt_servidor, (struct sockaddr*)&endereco, (socklen_t*)&addrlen)) < 0) {
+				printf("Erro de conexão com socket cliente\n");
+				exit(EXIT_FAILURE);
+			}
+			// Com a conexão feita agora realiza a leitura da mensagem enviada pelo cliente
+			leitura = read(novo_socket, buffer, 1024);	// lê a mensagem e armazena no buffer
+			printf("%s\n", buffer);	// exibe mensagem recebida
+			bzero(buffer, sizeof(buffer)); // zera espaço de mémoria para struct do endereço
+			send(novo_socket, msg, strlen(msg), 0);	// responde solicitação do cliente
+			printf("Hello message sent\n");
+			
+			// Fecha conexão com o socket
+			close(novo_socket);
+			// encerra o servidor
 		}
-
-		// Com a conexão feita agora realiza a leitura da mensagem enviada pelo cliente
-		leitura = read(novo_socket, buffer, 1024);	// lê a mensagem e armazena no buffer
-		printf("%s\n", buffer);	// exibe mensagem recebida
-		send(novo_socket, msg, strlen(msg), 0);	// responde solicitação do cliente
-		printf("Hello message sent\n");
 		
-		// Fecha conexão com o socket
-		close(novo_socket);
-		// encerra o servidor
-		shutdown(pt_servidor, SHUT_RDWR);
-
+	shutdown(pt_servidor, SHUT_RDWR);
 	return 0;
 }
