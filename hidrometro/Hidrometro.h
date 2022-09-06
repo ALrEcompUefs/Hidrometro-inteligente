acae1fe72900b4e17e3d4255a8d1b688cd0e7da4
Alisson Rodrigues <43976401+ALrEcompUefs@users.noreply.github.com>
2022-09-06 09:54:50 -0300

    Update Hidrometro.h

--- hidrometro/Hidrometro.h
+++ hidrometro/Hidrometro.h
@@ -1,7 +1,6 @@
-#ifndef HIDROMETRO_H
-#define HIDROMETRO_H
 #include <iostream>
 #include <string>
+#include <unistd.h>
 using namespace std;
 
 class Hidrometro
@@ -12,15 +11,31 @@ class Hidrometro
 		string cep; 		// cep da localidade
 		float consumo;		// consumo total do hidrometro
 		float vazao; 		// vazao por segundo do hidrometro
+		float vazamento;	// vazamento de água presente
 		bool bloqueado;		// estado do hidrometro 
 	public:
 		Hidrometro(int id,string cpf,string cep,float vazao);
-		void exibeMenssagem();
+		/// @brief Exibe atributos do hidrometro
+		void exibeMensagem();
+		
+		/// @brief Altera o indentificador dohidrometro
+		/// @param id 	novo id
 		void setId(int id);
+		/// @brief Altera o cpf do dono da Residencia do hidrometro
+		/// @param cpf 	novo cpf
 		void setCPF(string cpf);
+		/// @brief Altera cep da residencia do hidrometro
+		/// @param cep  novo cep
 		void setCep(string cep);
+		/// @brief Altera a vazão definida para o hidrometro
+		/// @param vazao vazão atual
 		void setVazao(float vazao); 
+		/// @brief Altera o vazamento no hidrometro
+		/// @param vazamento valor do vazamento
+		void setVazamento(float vazamento); 
+		/// @brief Bloqueia o hidrometro impedindo o fluxo de água
 		void bloquearHidrometo();
+		/// @brief Desbloqueia o hidrometro liberando o fluxo de água
 		void desbloquearHidrometo();
 		
 		int getId();
@@ -28,7 +43,8 @@ class Hidrometro
 		string getCep();
 		float getConsumo();
 		float getVazao();
+		float getVazamento();
 		bool estaBloqueado();
+		/// @brief Método para executar a thread que simula o funcionamento do hidrometro
+		void executarHidrometro();
 };
-
-#endif
